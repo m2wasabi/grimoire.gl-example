a88b7a9e7b3226f5f3e8aabdb4767819dd3f79a5
m2wasabi <m2wasabi@gmail.com>
2016-11-06 14:43:41 +0900

    Add rotate message

--- t10-01/index.js
+++ t10-01/index.js
@@ -1,11 +1,23 @@
-gr(function() {
-    var $$ = gr("#main");
-    gr.registerComponent("Print", {
+    gr.registerComponent("Rotate", {
         attributes: {
             test: {
                 defaultValue: "HELLO WORLD!",
                 converter: "String"
             }
+        },
+        $awake:()=>{
+          console.log(111);
+        },
+        $update:function(){
+            if(this.i === void 0)
+            {
+                this.i = 0;
+            }
+            this.i ++;
+            this.node.setAttribute("rotation",''+this.i+','+this.i+','+this.i+'');
+
         }
     });
+    gr.registerNode("rotate",["Rotate"],{},"mesh");
+gr(function() {
 });
